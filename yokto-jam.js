76fb6f2de7343fa1ac5b1b577bc3897856cd4409
Captain Nick Lucifer* <naryal2580@gmail.com>
2021-05-27 15:23:05 +0545

    Update yokto-jam.js

--- yokto-jam.js
+++ yokto-jam.js
@@ -1,16 +1,29 @@
-const $ = (query) => {return document.querySelector(query);};
-const $$ = (query) => {return document.querySelectorAll(query);};
-const $r = (callback) => { window.addEventListener("DOMContentLoaded", () => {callback();});};
+const $ = (query) => {
+    const elems = document.querySelectorAll(query);
+    if (elems.length === 1) {
+        return elems[0];
+    }
+    return elems;
+};
+const $$ = (callback) => {
+    window.addEventListener("DOMContentLoaded", () => {
+        callback();
+    });
+};
 const $_ = async (method, url, data) => {
+    var headers = {};
+    if (data) {
+        headers['Content-Type'] = 'application/json';
+    }
     const resp = await fetch(url, {
         method: method,
         mode: 'same-origin',
         cache: 'no-cache',
         credentials: 'same-origin',
-        headers: {'Content-Type': 'application/json'},
+        headers: headers,
         redirect: 'follow',
         referrerPolicy: 'no-referrer',
         body: JSON.stringify(data)
     });
     return await resp.json();
-}
+};
